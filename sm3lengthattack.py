763f55c929e2fcd1364a46d8ea535244495ef641
xiaoxiaosu <105589287+wangwangxiaoxiaosul@users.noreply.github.com>
2022-07-28 22:58:48 +0800

    Add files via upload

---
+++ sm3lengthattack.py
@@ -0,0 +1,86 @@
+
+from gmssl import sm3, func
+import random
+import struct
+import store
+
+secret = str(random.random())
+secret_hash = sm3.sm3_hash(func.bytes_to_list(bytes(secret, encoding='utf-8')))
+secret_len = len(secret)
+append_m = "201900210030"   # 随机的附加消息
+pad_str = ""
+pad = []
+
+def padding(msg):    #构造补充部分
+    mlen = len(msg)
+    msg.append(0x80)
+    mlen += 1
+    tail = mlen % 64
+    range_end = 56
+    if tail > range_end:
+        range_end = range_end + 64
+    for i in range(tail, range_end):
+        msg.append(0x00)
+    bit_len = (mlen - 1) * 8
+    msg.extend([int(x) for x in struct.pack('>q', bit_len)])
+    for j in range(int((mlen - 1) / 64) * 64 + (mlen - 1) % 64, len(msg)):
+        global pad
+        pad.append(msg[j])
+        global pad_str
+        pad_str += str(hex(msg[j]))
+    return msg
+
+
+
+def generate_guess_hash(old_hash, secret_len, append_m):
+    """
+       1.生成一个secret，算出secret的sm3值
+        2.根据sm3值分组后得到各个寄存器里的值
+        3.在secret+padding之后附加一段消息，用上一步寄存器里的值作为IV去加密附加的那段消息，得到m1
+        4.再用sm3去加密secret+padding+m'，得到m2
+        5.若m1和m2相等，则攻击成功
+    """
+    vectors = []
+    message = ""
+    # 将old_hash分组，每组8个字节, 并转换为整数
+    for r in range(0, len(old_hash), 8):
+        vectors.append(int(old_hash[r:r + 8], 16))
+
+    # 伪造消息
+    if secret_len > 64:
+        for i in range(0, int(secret_len / 64) * 64):
+            message += 'a'
+    for i in range(0, secret_len % 64):
+        message += 'a'
+    message = func.bytes_to_list(bytes(message, encoding='utf-8'))
+    message = padding(message)
+    message.extend(func.bytes_to_list(bytes(append_m, encoding='utf-8')))
+    return store.sm3_hash(message, vectors)
+
+
+
+
+guess_hash = generate_guess_hash(secret_hash, secret_len, append_m)
+new_msg = func.bytes_to_list(bytes(secret, encoding='utf-8'))
+new_msg.extend(pad)
+new_msg.extend(func.bytes_to_list(bytes(append_m, encoding='utf-8')))
+new_msg_str = secret + pad_str + append_m
+
+new_hash = sm3.sm3_hash(new_msg)
+
+
+print("secret: "+secret)
+print("secret length:%d" % len(secret))
+print("secret hash:" + secret_hash)
+print("附加消息:", append_m)
+print("-----------------------------------------------------")
+print("计算附加消息的值")
+print("hash_guess:" + guess_hash)
+print("-----------------------------------------------------")
+print("计算hash(secret+padding+m')")
+print("new message: \n" + new_msg_str)
+print("hash(new message):" + new_hash)
+if new_hash == guess_hash:
+    print("success!")
+else:
+    print("fail..")
