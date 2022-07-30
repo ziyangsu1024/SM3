bf9f3a0cefbc37f11f2b5c88b73cdf279f39601d
ziyangsu1024 <105589123+ziyangsu1024@users.noreply.github.com>
2022-07-31 00:02:49 +0800

    Update pollard_rho.py

--- pollard_rho.py
+++ pollard_rho.py
@@ -3,6 +3,37 @@ import time
 from functools import reduce
 from random import randint,seed
 import math
+
+def quick_pow(x, y, p): #素性检测
+    ans = 1
+    while y:
+        if y & 1:
+            ans = ans * x % p
+        x = x * x % p
+        y >>= 1
+    return ans
+
+def miller_rabin(n, test_time = 8):
+    if n < 3:
+        return n == 2
+    u = n - 1
+    t = 0
+    while u % 2 == 0:
+        u //= 2
+        t += 1
+    for i in range(1, test_time + 1):
+        x = randint(2, n - 1)
+        v = quick_pow(x, u, n)
+        if v == 1 or v == n - 1:
+            continue
+        for j in range(t + 1):
+            v = v * v % n
+            if v == n - 1:
+                break
+        else:
+            return False
+    return True
+
 def Pollard_RHO(p, c): #pollard_rho 算法
     (i,k)=(1,2)
     x=randint(0,p)
